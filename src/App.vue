7fc7a2d92030bda0ee9efe66e97d1ece58d9f4a2
sansil <sansil32@gmail.com>
2019-02-12 16:29:03 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,216 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="mark-app">
+    <div class="contendor-app-ppal"></div>
+    <div class="backround-color-ppal"></div>
+    <div class="loader-cont" v-if="loading">
+      <div class="mark-loader"></div>
+      <div class="spinner">
+        <h1>Conectando...</h1>
+        <rise-loader color="#009de8" :loading="loading" size="2rem"></rise-loader>
+      </div>
+    </div>
+    <div class="field drop-down-switch" v-if="!conectado && !loading">
+      <input
+        id="switchExample"
+        type="checkbox"
+        class="switch is-info"
+        :checked="checked"
+        @click="checked=!checked"
+      >
+      <label style="color:white;" for="switchExample">
+        <b>MANUAL</b>
+      </label>
+    </div>
+    <div class="contenido">
+      <!-- <drop-down></drop-down> -->
+      <div class="m-input w3-border" v-if="!conectado && !loading">
+        <input type="text" placeholder="ESCRIBIR PUERTO" v-model="puerto" :disabled="!checked">
+        <a @click="onConectar" class="button is-info is-outlined" style="border-radius:0;">CONECTAR</a>
+      </div>
+      <div class="conectado" v-if="conectado">
+        <section class="hero">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">Conectado</h1>
+              <!-- <h2 class="subtitle">Hero subtitle</h2> -->
+            </div>
+          </div>
+        </section>
+        <a
+          @click="onDesconectar"
+          class="button is-info is-outlined"
+          style="border-radius:0;"
+        >DESCONECTAR</a>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+require("@/assets/sass/main.scss");
+
+// import DropDown from '@/components/DropDown.vue';
+import axios from "axios";
+import RiseLoader from "vue-spinner/src/RiseLoader.vue";
+
+export default {
+  components: {
+    // DropDown
+    RiseLoader
+  },
+  data() {
+    return {
+      puerto: "",
+      loading: false,
+      conectado: false,
+      checked: false
+    };
+  },
+  methods: {
+    onDesconectar() {
+      axios
+        .post("http://localhost:8081/desconectar", {})
+        .then(response => {
+          console.log(response);
+          this.loading = false;
+          this.conectado = false;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    onConectar() {
+      console.log("conectando");
+      this.loading = true;
+      var connection_port = this.puerto;
+      if (!checked) connection_port = "";
+      axios
+        .post("http://localhost:8081/conectar", {
+          puerto: connection_port
+        })
+        .then(response => {
+          console.log(response);
+          this.loading = false;
+          this.conectado = true;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url("https://fonts.googleapis.com/css?family=Inconsolata|Ubuntu");
+
+html {
+  box-sizing: border-box;
+}
+.mark-app {
+}
+.contendor-app-ppal {
+  background-image: url("./assets/background-img.jpg");
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover; /* Resize the background image to cover the entire container */
+  background-position: center right;
+  height: 100%;
+  width: 100%;
+  opacity: 0.7;
+  position: fixed;
+  z-index: -2;
+}
+.drop-down-switch {
+  position: absolute;
+  top: 3rem;
+  left: 3rem;
+  display: inline;
+  margin: 0;
+  margin-right: 2rem;
+}
+.drop-down-switch label {
+  font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    "Helvetica", "Arial", sans-serif;
+}
+
+.loader-cont {
+  display: flex;
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+}
+
+.loader-cont h1 {
+  font-size: 1.2rem;
+  font-weight: 400;
+  padding: 2rem;
+  z-index: 300;
+  color: whitesmoke;
+}
+.spinner {
+  z-index: 300;
+  opacity: 1;
+}
+.mark-loader {
+  z-index: 200;
+  width: 100%;
+  position: fixed;
+  height: 100vh;
+  background-color: rgb(131, 131, 131);
+  opacity: 0.6;
+  position: fixed;
+}
+
+.conectado {
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.conectado h1,
+a {
+  color: white;
+  display: inline;
+  margin-left: 1rem;
+}
+.backround-color-ppal {
+  background-color: #193f5d;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: -1;
+  opacity: 0.8;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.contenido {
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100vh;
+}
+
+.m-input input {
+  height: 2rem;
+  padding: 1rem;
+  box-sizing: border-box;
+  margin-right: 1rem;
+  /* width: 50%; */
+  /* border-radius: 0.5rem; */
+}
+
+.m-input input:focus {
+  height: 2rem;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-color: #009de8;
+  box-shadow: none;
+  outline-color: #009de8;
+  /* border-radius: 0.5rem; */
 }
 </style>
---
+++ src/components/DropDown.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="dropdown" :class="{'is-active': isActive }">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu"  @click="isActive = !isActive">
+      <span>Seleccionar puerto</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu" role="menu">
+    <div class="dropdown-content">
+      <a href="#" class="dropdown-item">
+        Dropdown item
+      </a>
+      <a class="dropdown-item">
+        Other dropdown item
+      </a>
+      <a href="#" class="dropdown-item is-active">
+        Active dropdown item
+      </a>
+      <a href="#" class="dropdown-item">
+        Other dropdown item
+      </a>
+      <hr class="dropdown-divider">
+      <a href="#" class="dropdown-item">
+        With a divider
+      </a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+require('@/assets/sass/main.scss');
+  export default {
+    data() {
+      return {
+        isActive: false
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
